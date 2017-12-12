d24b6eb3f6753c0d052e5f2ec11198bef887733a
toto <toto6321@qq.com>
2017-12-12 13:31:54 +0800

    #1 try to make the login works but encounter exception in axios.post(url,login) Todos:#1 connect frontend and backend

--- API/controllers/user_query.js
+++ API/controllers/user_query.js
@@ -7,60 +7,60 @@
 const knex = require('../../app_backend/config/postgresql_configuration')
 
 const getAll = () => {
-  'use strict'
-  return knex('users')
-    .select('*')
+	'use strict'
+	return knex('users')
+		.select('*')
 }
 
 const getOne = (uid) => {
-  'use strict'
-  return knex('users')
-    .select('*')
-    .where({id: parseInt(uid)})
+	'use strict'
+	return knex('users')
+		.select('*')
+		.where({id: parseInt(uid)})
 }
 
 const insertOne = (newUser) => {
-  'use strict'
-  return knex('users')
-    .insert(newUser)
-    .returning('*')
+	'use strict'
+	return knex('users')
+		.insert(newUser)
+		.returning('*')
 }
 
 const updateOne = (uid, newUser) => {
-  'use strict'
-  return knex('users')
-    .update(newUser)
-    .where({id: parseInt(uid)})
-    .returning('*')
+	'use strict'
+	return knex('users')
+		.update(newUser)
+		.where({id: parseInt(uid)})
+		.returning('*')
 }
 
 const deleteOne = (uid) => {
-  'use strict'
-  return knex('users')
-    .del()
-    .where({id: parseInt(uid)})
-    .returning('*')
+	'use strict'
+	return knex('users')
+		.del()
+		.where({id: parseInt(uid)})
+		.returning('*')
 }
 
 const checkLogin = (emailOrMobile, pwd) => {
-  'use strict'
-  // two ways to check:
-  // one is to get pwd according the email or mobile and compare with parameter pwd here,
-  // the other is to query if such a user exists in the database that has matched email/phone and pwd
-  // here we apply the second one
-  const id = knex('users')
-    .select('id', 'name', 'mobile', 'pwd')
-    .where({'email': emailOrMobile, 'pwd': pwd})
-    .orWhere({'mobile': emailOrMobile, 'pwd': pwd})
-  console.log('id', id)
-  return id
+	'use strict'
+	// two ways to check:
+	// one is to get pwd according the email or mobile and compare with parameter pwd here,
+	// the other is to query if such a user exists in the database that has matched email/phone and pwd
+	// here we apply the second one
+	const result = knex('users')
+		.select('id', 'name', 'mobile', 'pwd')
+		.where({'email': emailOrMobile, 'pwd': pwd})
+		.orWhere({'mobile': emailOrMobile, 'pwd': pwd})
+	// console.log('result', result)
+	return result
 }
 
 module.exports = {
-  getAll,
-  getOne,
-  insertOne,
-  updateOne,
-  deleteOne,
-  checkLogin
+	getAll,
+	getOne,
+	insertOne,
+	updateOne,
+	deleteOne,
+	checkLogin
 }
--- app_backend/controllers/index.js
+++ app_backend/controllers/index.js
@@ -8,62 +8,98 @@ const reqOption = require('../config/requestOptionsConfiguration')
 const axios = require('axios')
 
 module.exports.getIndex = async ctt => {
-  'use strict'
-  /* ctx.body = {
+	'use strict'
+	/* ctx.body = {
 		 msg: 'get / from controller'
 	 }
 	*/
-  await ctt.render('index')
+	await ctt.render('index')
 }
 
-module.exports.app = async ctt => {
-  'use strict'
-  let {htmlFile} = ctt.params
-  let index = htmlFile.lastIndexOf('.')
-  index === -1 ? index = htmlFile.length : index = index
-  console.log('index: ', index)
-  const htmlFileName = htmlFile.substring(0, index)
-  await ctt.render(htmlFileName)
+module.exports.getLogin = async ctt => {
+	"use strict";
+	try {
+		await ctt.render('login')
+	} catch (e) {
+		ctt.throw(400, e)
+	}
 }
+
+module.exports.getSignup = async ctt => {
+	"use strict";
+	try {
+		await ctt.render('signup')
+	} catch (e) {
+		ctt.throw(400, e)
+	}
+}
+
+/*module.exports.app = async ctt => {
+	'use strict'
+	let {htmlFile} = ctt.params
+	let index = htmlFile.lastIndexOf('.')
+	index === -1 ? index = htmlFile.length : index = index
+	console.log('index: ', index)
+	const htmlFileName = htmlFile.substring(0, index)
+	await ctt.render(htmlFileName)
+}*/
 
 module.exports.login = async ctt => {
-  'use strict'
-  ctt.redirect('/app', {
-    name: 'toto',
-    gender: 'male',
-    email: 'toto@qq.com'
-  }
-  )
+	'use strict'
+	const login = {
+		emailOrMobile: ctt.request.body.emailOrMobile,
+		pwd: ctt.request.body.pwd
+	}
+	console.log('login', login)
+	try {
+		const url = reqOption.url + '/api/v1/user/login'
+		const res = await axios({
+			url: url,
+			method: 'post',
+			data: login
+		})
+		console.log(res.data)
+		if (res.length > 0) {
+			// ctt.redirect(`/app/${id}/home`)
+			ctt.render('home', res.data)
+		} else {
+			ctt.redirect('app/login')
+		}
+	} catch (e) {
+		// ctt.throw(400, e)
+		ctt.status = 400
+		ctt.body = {msg: 'failed to post /api/v1/user/login'}
+	}
 }
 
 module.exports.signup = async ctt => {
-  'use strict'
-  ctt.redirect('/app/login')
+	'use strict'
+	ctt.redirect('/app/login')
 }
 
 module.exports.getHome = async ctt => {
-  'use strict'
-  const {uid} = ctt.params
-  const url = reqOption.url + `/user/${uid}`
-  // console.log(requestOption.url)
-  try {
-    const res = await axios.get(url)
-    if (res) {
-      ctt.status = parseInt(res.status)
-      const user = res.data[0]
-      // console.log('user:', user)
-      await ctt.render('home', user)
-    } else {
-      ctt.status = 400
-    }
-  } catch (e) {
-    ctt.throw(400, e)
-  }
+	'use strict'
+	const {uid} = ctt.params
+	const url = reqOption.url + `/user/${uid}`
+	// console.log(requestOption.url)
+	try {
+		const res = await axios.get(url)
+		if (res) {
+			ctt.status = parseInt(res.status)
+			const user = res.data[0]
+			// console.log('user:', user)
+			await ctt.render('home', user)
+		} else {
+			ctt.status = 400
+		}
+	} catch (e) {
+		ctt.throw(400, e)
+	}
 
 // ctt.redirect('/app/login', res)
 }
 
 module.exports.logout = async ctt => {
-  'use strict'
-  ctt.redirect('/app/index')
+	'use strict'
+	ctt.redirect('/app/index')
 }
