caa7202036f99b05ba0c1db5af50d9307af40d9d
toto <toto6321@qq.com>
2017-12-14 13:53:23 +0800

    #1 fix the problem that axios.post(url,login) throws an exception, because of the wrong url that it contain redundant /api/v1 Todos:#1 connect frontend and backend

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -7,139 +7,132 @@
 const query = require('./user_query')
 
 module.exports.readAll = async ctt => {
-	'use strict'
-	try {
-		const data = await query.getAll()
-		if (data) {
-			ctt.status = 200
-			ctt.body = data
-		} else {
-			ctt.status = 400
-			ctt.body = {
-				code: 1,
-				msg: 'failed to get users'
-			}
-		}
-	} catch (e) {
-		ctt.throw(400, e)
-	}
+  'use strict'
+  try {
+    const data = await query.getAll()
+    if (data) {
+      ctt.status = 200
+      ctt.body = data
+    } else {
+      ctt.status = 400
+      ctt.body = {
+        code: 1,
+        msg: 'failed to get users'
+      }
+    }
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 }
 
 module.exports.readOne = async ctt => {
-	'use strict'
-	const {uid} = ctt.params
-	try {
-		const data = await query.getOne(uid)
-		if (data) {
-			ctt.status = 200
-			ctt.body = data
-		} else {
-			ctt.status = 400
-			ctt.body = {
-				code: 2,
-				msg: 'failed to get a user'
-			}
-		}
-	} catch (e) {
-		ctt.throw(400, e)
-	}
+  'use strict'
+  const {uid} = ctt.params
+  try {
+    const data = await query.getOne(uid)
+    if (data) {
+      ctt.status = 200
+      ctt.body = data
+    } else {
+      ctt.status = 400
+      ctt.body = {
+        code: 2,
+        msg: 'failed to get a user'
+      }
+    }
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 }
 
 module.exports.createOne = async ctt => {
-	'use strict'
-	let {name, gender, email, mobile, pwd} = ctt.request.body
-	// todo data filter and format
-	const newUser = {
-		name: name,
-		gender: gender,
-		email: email,
-		mobile: mobile,
-		pwd: pwd
-	}
-	console.log('newUser:', newUser)
-	try {
-		const data = await query.insertOne(newUser)
-		if (data) {
-			ctt.status = 200
-			ctt.body = data
-		} else {
-			ctt.status = 400
-			ctt.body = {
-				code: 2,
-				msg: 'failed to get a user'
-			}
-		}
-	} catch (e) {
-		ctt.throw(400, e)
-	}
+  'use strict'
+  let {name, gender, email, mobile, pwd} = ctt.request.body
+  // todo data filter and format
+  const newUser = {
+    name: name,
+    gender: gender,
+    email: email,
+    mobile: mobile,
+    pwd: pwd
+  }
+  console.log('newUser:', newUser)
+  try {
+    const data = await query.insertOne(newUser)
+    if (data) {
+      ctt.status = 200
+      ctt.body = data
+    } else {
+      ctt.status = 400
+      ctt.body = {
+        code: 2,
+        msg: 'failed to get a user'
+      }
+    }
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 }
 
 module.exports.updateOne = async ctt => {
-	'use strict'
-	let {uid} = ctt.params
-	let {name, gender, email, mobile, pwd} = ctt.request.body
-	// todo data filter and format
-	const newUser = {
-		name: name,
-		gender: gender,
-		email: email,
-		mobile: mobile,
-		pwd: pwd
-	}
-	console.log('updateUser:', newUser)
-	try {
-		const data = await query.updateOne(uid, newUser)
-		if (data) {
-			ctt.status = 200
-			ctt.body = data
-		} else {
-			ctt.status = 400
-			ctt.body = {
-				code: 2,
-				msg: 'failed to get a user'
-			}
-		}
-	} catch (e) {
-		ctt.throw(400, e)
-	}
+  'use strict'
+  let {uid} = ctt.params
+  let {name, gender, email, mobile, pwd} = ctt.request.body
+  // todo data filter and format
+  const newUser = {
+    name: name,
+    gender: gender,
+    email: email,
+    mobile: mobile,
+    pwd: pwd
+  }
+  console.log('updateUser:', newUser)
+  try {
+    const data = await query.updateOne(uid, newUser)
+    if (data) {
+      ctt.status = 200
+      ctt.body = data
+    } else {
+      ctt.status = 400
+      ctt.body = {
+        code: 2,
+        msg: 'failed to get a user'
+      }
+    }
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 }
 
 module.exports.deleteOne = async ctt => {
-	'use strict'
-	let {uid} = ctt.params
-	try {
-		const data = await query.deleteOne(uid)
-		if (data) {
-			ctt.status = 200
-			ctt.body = data
-		} else {
-			ctt.status = 400
-			ctt.body = {
-				code: 2,
-				msg: 'failed to get a user'
-			}
-		}
-	} catch (e) {
-		ctt.throw(400, e)
-	}
+  'use strict'
+  let {uid} = ctt.params
+  try {
+    const data = await query.deleteOne(uid)
+    if (data) {
+      ctt.status = 200
+      ctt.body = data
+    } else {
+      ctt.status = 400
+      ctt.body = {
+        code: 2,
+        msg: 'failed to get a user'
+      }
+    }
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 }
 
 module.exports.login = async ctt => {
-	'use strict'
-	const {emailOrMobile, pwd} = ctt.request.body
-	console.log('before query', emailOrMobile)
-	try {
-		const result = await query.checkLogin(emailOrMobile, pwd)
-		ctt.status = 200
-		console.log('result ', result)
-		ctt.body = result
-		/*   if (result && result.length > 0) {
-				 const uid = result.id
-				 // ctt.redirect(`/app/${uid}/home`)
-			 } else {
-				 ctt.redirect('/app/login')// email or mobile doesn't match the pwd
-			 }*/
-	} catch (e) {
-		ctt.throw(400, e)
-	}
+  'use strict'
+  const {emailOrMobile, pwd} = ctt.request.body
+  try {
+    const result = await query.checkLogin(emailOrMobile, pwd)
+    ctt.status = 200
+    // console.log('result ', result)
+    ctt.body = result
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 }
--- app_backend/controllers/index.js
+++ app_backend/controllers/index.js
@@ -8,33 +8,33 @@ const reqOption = require('../config/requestOptionsConfiguration')
 const axios = require('axios')
 
 module.exports.getIndex = async ctt => {
-	'use strict'
-	/* ctx.body = {
+  'use strict'
+  /* ctx.body = {
 		 msg: 'get / from controller'
 	 }
 	*/
-	await ctt.render('index')
+  await ctt.render('index')
 }
 
 module.exports.getLogin = async ctt => {
-	"use strict";
-	try {
-		await ctt.render('login')
-	} catch (e) {
-		ctt.throw(400, e)
-	}
+  'use strict'
+  try {
+    await ctt.render('login')
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 }
 
 module.exports.getSignup = async ctt => {
-	"use strict";
-	try {
-		await ctt.render('signup')
-	} catch (e) {
-		ctt.throw(400, e)
-	}
+  'use strict'
+  try {
+    await ctt.render('signup')
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 }
 
-/*module.exports.app = async ctt => {
+/* module.exports.app = async ctt => {
 	'use strict'
 	let {htmlFile} = ctt.params
 	let index = htmlFile.lastIndexOf('.')
@@ -42,64 +42,59 @@ module.exports.getSignup = async ctt => {
 	console.log('index: ', index)
 	const htmlFileName = htmlFile.substring(0, index)
 	await ctt.render(htmlFileName)
-}*/
+} */
 
 module.exports.login = async ctt => {
-	'use strict'
-	const login = {
-		emailOrMobile: ctt.request.body.emailOrMobile,
-		pwd: ctt.request.body.pwd
-	}
-	console.log('login', login)
-	try {
-		const url = reqOption.url + '/api/v1/user/login'
-		const res = await axios({
-			url: url,
-			method: 'post',
-			data: login
-		})
-		console.log(res.data)
-		if (res.length > 0) {
-			// ctt.redirect(`/app/${id}/home`)
-			ctt.render('home', res.data)
-		} else {
-			ctt.redirect('app/login')
-		}
-	} catch (e) {
-		// ctt.throw(400, e)
-		ctt.status = 400
-		ctt.body = {msg: 'failed to post /api/v1/user/login'}
-	}
+  'use strict'
+  const login = {
+    emailOrMobile: ctt.request.body.emailOrMobile,
+    pwd: ctt.request.body.pwd
+  }
+  console.log('login', login)
+  try {
+    const url = reqOption.url + '/user/login'
+    const res = await axios.post(url, login)
+    console.log(res.data)
+    if (res.data.length > 0) {
+      // ctt.redirect(`/app/${id}/home`)
+      const user = res.data[0]
+      await ctt.render('home', user)
+    } else {
+      ctt.redirect('/app/login')
+    }
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 }
 
 module.exports.signup = async ctt => {
-	'use strict'
-	ctt.redirect('/app/login')
+  'use strict'
+  ctt.redirect('/app/login')
 }
 
 module.exports.getHome = async ctt => {
-	'use strict'
-	const {uid} = ctt.params
-	const url = reqOption.url + `/user/${uid}`
-	// console.log(requestOption.url)
-	try {
-		const res = await axios.get(url)
-		if (res) {
-			ctt.status = parseInt(res.status)
-			const user = res.data[0]
-			// console.log('user:', user)
-			await ctt.render('home', user)
-		} else {
-			ctt.status = 400
-		}
-	} catch (e) {
-		ctt.throw(400, e)
-	}
+  'use strict'
+  const {uid} = ctt.params
+  const url = reqOption.url + `/user/${uid}`
+  // console.log(requestOption.url)
+  try {
+    const res = await axios.get(url)
+    if (res) {
+      ctt.status = parseInt(res.status)
+      const user = res.data[0]
+      // console.log('user:', user)
+      await ctt.render('home', user)
+    } else {
+      ctt.status = 400
+    }
+  } catch (e) {
+    ctt.throw(400, e)
+  }
 
 // ctt.redirect('/app/login', res)
 }
 
 module.exports.logout = async ctt => {
-	'use strict'
-	ctt.redirect('/app/index')
+  'use strict'
+  ctt.redirect('/app/index')
 }
