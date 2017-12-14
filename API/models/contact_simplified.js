92584b4094ee7eaea73097c3f6f02da8292fdb8c
toto <toto6321@qq.com>
2017-12-14 15:46:24 +0800

    #1 to be able to show the contacts of a user Todos:#1 connect frontend and backend

--- API/models/contact_simplified.js
+++ API/models/contact_simplified.js
@@ -6,10 +6,10 @@
 
 const mongoose = require('mongoose')
 const contactSchema = new mongoose.Schema({
-	name: String,
-	phone: String,
-	address: String,
-	uid: Number
+  name: String,
+  phone: String,
+  address: String,
+  uid: Number
 })
 
 module.exports = mongoose.model('Contact1', contactSchema)
